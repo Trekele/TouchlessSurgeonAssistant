1d6b514c565026861438b176fe5abce48c13253d
Trekele <trekele@gmail.com>
2016-04-09 15:28:33 -0400

    New screen
    Added a bunch of new screens

---
+++ TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/winOperation.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TouchlessSurgeonAssistant
+{
+    /// <summary>
+    /// Interaction logic for winOperation.xaml
+    /// </summary>
+    public partial class winOperation : Window
+    {
+        public winOperation()
+        {
+            InitializeComponent();
+        }
+    }
+}

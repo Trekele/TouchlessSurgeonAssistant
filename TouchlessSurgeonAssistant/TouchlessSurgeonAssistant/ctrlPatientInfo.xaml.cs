ebf3c7f9d9f33f2914a1aadf1a5cf026e159a3f2
Trekele <trekele@gmail.com>
2016-04-09 17:29:56 -0400

    Setup Timer control

--- TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/ctrlPatientInfo.xaml.cs
+++ TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/ctrlPatientInfo.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TouchlessSurgeonAssistant
 {
@@ -20,9 +21,43 @@ namespace TouchlessSurgeonAssistant
     /// </summary>
     public partial class ctrlPatientInfo : UserControl
     {
+        private DispatcherTimer timer;
+        private int totalSeconds;
         public ctrlPatientInfo()
         {
             InitializeComponent();
+            //disable the endButton
+            btnEnd.IsEnabled = true;
+
+            timer = new DispatcherTimer();
+            timer.Tick += dispatcherTimer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            //disable the start button and enable the end button
+            btnStart.IsEnabled = false;
+            btnEnd.IsEnabled = true;
+
+            //start timing
+            timer.Start();
+
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e)
+        {
+            //disable the end button and enable the start button
+            btnStart.IsEnabled = true;
+            btnEnd.IsEnabled = false;
+
+            timer.Stop();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            totalSeconds++;
+            // code goes here
+            lblTimer.Content = string.Format("{0}:0{1}:{2}", totalSeconds / 86400, totalSeconds / 60, totalSeconds % 60);
         }
     }
 }

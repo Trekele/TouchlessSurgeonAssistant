6105901843eaad2fed2bd26158dcc37bd47f7400
Trekele <trekele@gmail.com>
2016-04-09 21:07:01 -0400

    Added database and patient class

---
+++ TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/PatientClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+
+namespace TouchlessSurgeonAssistant
+{
+    class PatientClass
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime dob { get; set; }
+
+        public PatientClass(int id, string firstName, string middleName, string lastName, DateTime dob)
+        {
+            this.ID = id;
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            this.dob = dob;
+        }
+
+        public PatientClass(SqlCeDataReader rdr)
+        {
+            this.ID = rdr.GetInt32(0);
+            this.FirstName = rdr.GetString(1);
+            this.MiddleName = rdr.GetString(2);
+            this.LastName = rdr.GetString(3);
+            this.dob = rdr.GetDateTime(4);
+        }
+    }
+}
--- TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/winDashboard.xaml.cs
+++ TouchlessSurgeonAssistant/TouchlessSurgeonAssistant/winDashboard.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlServerCe;
+using System.Data;
 
 namespace TouchlessSurgeonAssistant
 {
@@ -22,6 +24,47 @@ namespace TouchlessSurgeonAssistant
         public winDashboard()
         {
             InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //declare sqlce objects
+            SqlCeConnection conn = null;
+            SqlCeCommand cmd = null;
+            SqlCeDataReader rdr = null;
+            SqlCeDataAdapter da = null;
+
+            //create list of patients
+            List<PatientClass> list_patients = new List<PatientClass>();
+
+            try
+            {
+                //path to the database
+                string connString = "Data Source = PatientData.sdf";
+                conn = new SqlCeConnection(connString);
+                conn.Open();
+
+                //query to run on the database
+                string selectCmd = "SELECT * FROM PatientInfo";
+                cmd = conn.CreateCommand();
+                cmd.CommandText = selectCmd;
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    list_patients.Add(new PatientClass(rdr));
+                }
+                patientDataGrid.DataContext = list_patients;
+                patientDataGrid.ItemsSource = list_patients;
+            }
+            catch
+            {
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
